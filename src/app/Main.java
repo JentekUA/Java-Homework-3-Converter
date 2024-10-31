aed5f1300f250504eded413694483f3591df066f
oleksandr.aleksashyn <oleksandr.aleksashyn@gmail.com>
2024-10-31 17:19:11 +0200

    Add logic for Celsius to Fahrenheit conversion

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,12 +7,18 @@ public class Main {
         System.out.println("Version 1.0.");
 
         double fahrenheit = 5;
+        double celsius = 12;
         double convertedCelsius = convFahrenheitToCelsius(fahrenheit);
+        double convertedFahrenheit = convCelsiusFahrenheit(celsius);
 
-        System.out.println("Result is " + convertedCelsius + " Celsius.");
+        System.out.println("Result is " + convertedCelsius + " Celsius and " + convertedFahrenheit + " Fahrenheit.");
     }
 
     private static double convFahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
+    }
+
+    private static double convCelsiusFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
     }
 }
